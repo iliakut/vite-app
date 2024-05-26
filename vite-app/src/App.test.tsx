04a062169978811c2ea03060bf9589c357789ce7
ilakutyrev <ilyakut@gmail.com>
2024-05-26 22:12:13 +0200

    test

--- vite-app/src/App.test.tsx
+++ vite-app/src/App.test.tsx
@@ -8,6 +8,7 @@ describe('App', () => {
 
   it('renders the App and Header components, calls fetch', () => {
     render(<App />)
+    
     expect(screen.getByText(/App header/i))
     expect(screen.getByText(/Click on the Vite and React logos to learn more/i))
     expect(global.fetch).toHaveBeenCalledTimes(1);
